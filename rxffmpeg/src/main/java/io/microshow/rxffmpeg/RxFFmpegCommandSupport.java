a28384a99fbce1cfda4611bba786201eecbe260c
Super <986386695@qq.com>
2020-05-29 07:43:12 +0800

    RxFFmpegCommandList构建命令build(boolean isLog)新增isLog,控制构建后是否输出命令行日志信息

--- rxffmpeg/src/main/java/io/microshow/rxffmpeg/RxFFmpegCommandSupport.java
+++ rxffmpeg/src/main/java/io/microshow/rxffmpeg/RxFFmpegCommandSupport.java
@@ -1,21 +1,32 @@
 package io.microshow.rxffmpeg;
 
+import android.text.TextUtils;
+
 /**
  * RxFFmpeg 命令支持
  * Created by Super on 2019/4/5.
  */
 public class RxFFmpegCommandSupport {
 
-    public static String[] getBoxblur() {
+    /**
+     * 给视频加毛玻璃效果
+     *
+     * @param inputPath  输入视频文件
+     * @param outputPath 输出视频文件
+     * @param boxblur    blur效果调节，默认 "5:1"
+     * @param isLog      true: 构建命令后 Log打印命令日志;  false :不打印命令日志
+     * @return cmds
+     */
+    public static String[] getBoxblur(String inputPath, String outputPath, String boxblur, boolean isLog) {
         RxFFmpegCommandList cmdlist = new RxFFmpegCommandList();
         cmdlist.append("-i");
-        cmdlist.append("/storage/emulated/0/1/input.mp4");
+        cmdlist.append(inputPath);
         cmdlist.append("-vf");
-        cmdlist.append("boxblur=5:1");
+        cmdlist.append("boxblur=" + (TextUtils.isEmpty(boxblur) ? "5:1" : boxblur));
         cmdlist.append("-preset");
         cmdlist.append("superfast");
-        cmdlist.append("/storage/emulated/0/1/result.mp4");
-        return cmdlist.build();
+        cmdlist.append(outputPath);
+        return cmdlist.build(isLog);
     }
 
 
