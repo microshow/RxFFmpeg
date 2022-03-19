e67a247eeec2900591b461105b799d235759bb63
Super <986386695@qq.com>
2022-03-19 17:40:09 +0800

    DEMO修改权限申请

--- app/src/main/java/io/microshow/rxffmpeg/app/activity/LaunchActivity.java
+++ app/src/main/java/io/microshow/rxffmpeg/app/activity/LaunchActivity.java
@@ -1,11 +1,7 @@
 package io.microshow.rxffmpeg.app.activity;
 
-import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,7 +12,6 @@ import io.microshow.rxffmpeg.app.utils.Utils;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
 
 /**
  * 启动页
@@ -26,38 +21,17 @@ public class LaunchActivity extends BaseActivity {
 
     private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
-    //权限
-    private RxPermissions rxPermissions = null;
-
-    //需要申请的权限，必须先在AndroidManifest.xml有声明，才可以动态获取权限
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_PHONE_STATE};
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         DataBindingUtil.setContentView(this, R.layout.activity_launch);
 
-        rxPermissions = new RxPermissions(this);
-
-        mCompositeDisposable.add(rxPermissions.request(PERMISSIONS_STORAGE).subscribe(new Consumer<Boolean>() {
-            @Override
-            public void accept(Boolean aBoolean) throws Exception {
-                if (aBoolean) {// 用户同意了权限
-                    gotoMainAct();
-                } else {//用户拒绝了权限
-                    Toast.makeText(LaunchActivity.this, "您拒绝了权限，请往设置里开启权限", Toast.LENGTH_LONG).show();
-                    finish();
-                }
-            }
-        }));
+        gotoMainAct();
     }
 
     private void gotoMainAct() {
-        mCompositeDisposable.add(Flowable.timer(3000, TimeUnit.MILLISECONDS)
+        mCompositeDisposable.add(Flowable.timer(2000, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(next -> {
                     startActivity(new Intent(LaunchActivity.this, MainActivity.class));
--- app/src/main/java/io/microshow/rxffmpeg/app/fragment/HomeFragment.java
+++ app/src/main/java/io/microshow/rxffmpeg/app/fragment/HomeFragment.java
@@ -1,11 +1,15 @@
 package io.microshow.rxffmpeg.app.fragment;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 import com.baidu.mobstat.StatService;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.lang.ref.WeakReference;
 
@@ -16,6 +20,8 @@ import io.microshow.rxffmpeg.app.R;
 import io.microshow.rxffmpeg.app.databinding.FragmentHomeBinding;
 import io.microshow.rxffmpeg.app.utils.CPUUtils;
 import io.microshow.rxffmpeg.app.utils.Utils;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
 
 /**
  * 首页
@@ -30,10 +36,27 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding> implements V
 
     private MyRxFFmpegSubscriber myRxFFmpegSubscriber;
 
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+    //权限
+    private RxPermissions rxPermissions = null;
+    //需要申请的权限，必须先在AndroidManifest.xml有声明，才可以动态获取权限
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.button) {
-            runFFmpegRxJava();
+            mCompositeDisposable.add(rxPermissions.request(PERMISSIONS_STORAGE).subscribe(new Consumer<Boolean>() {
+                @Override
+                public void accept(Boolean aBoolean) throws Exception {
+                    if (aBoolean) {// 用户同意了权限
+                        runFFmpegRxJava();
+                    } else {//用户拒绝了权限
+                        Toast.makeText(getActivity(), "您拒绝了权限，请往设置里开启权限", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }));
         }
     }
 
@@ -78,6 +101,7 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding> implements V
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        rxPermissions = new RxPermissions(this);
     }
 
     @Override
@@ -90,6 +114,7 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding> implements V
         init();
     }
 
+    @SuppressLint("SetTextI18n")
     private void init() {
         binding.editText.setText("ffmpeg -y -i /storage/emulated/0/1/input.mp4 -vf boxblur=5:1 -preset superfast /storage/emulated/0/1/result.mp4");
         binding.button.setOnClickListener(this);
@@ -164,6 +189,7 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding> implements V
         if (myRxFFmpegSubscriber != null) {
             myRxFFmpegSubscriber.dispose();
         }
+        mCompositeDisposable.clear();
     }
 
 }
